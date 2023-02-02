0cbaccc56950a7da63b00b0cb3c30c27c393434e
sarmad0324 <sarmadirfan78@gmail.com>
2023-02-02 23:57:16 +0500

    python added

---
+++ python/new.py
@@ -0,0 +1,48 @@
+#intro
+#a=input("What is your name : ")
+#print(f"Hello, {a}")
+
+#conditional
+#num=int(input("Enter a number : "))
+
+#if num<0:
+    #print("Negative number")
+#elif num>0:
+    #print("Positive number")
+#else:
+    #print("number is zero")
+
+#data structures
+#list
+#l=["sarmad","malaika","kashaf","sarmad"]
+#print(l)
+#print(type(l))
+#l.sort()
+#print(l)
+#print(l[0])
+#tuple
+#t=("sarmad","malaika")
+#print(t)
+#print(type(t))
+#print(t[0])
+#sets
+#s={"sarmad","malaika","kashaf",}
+#print(s)
+#print(type(s))
+
+#list
+#d={"sarmad":424,"malaika":370,"kashaf":379}
+#print(d)
+#print(type(d))
+#print(d["sarmad"])
+
+#loops
+for l in range(10):
+    print(l)
+
+#functions
+def sq(x):
+    return x*x
+
+for i in range(5):
+    print(f"The square of number {i} is {sq(i)} ")
---
+++ python/oop.py
@@ -0,0 +1,30 @@
+#class Point():
+#def __init__(self,a,b):
+        #self.x=a
+#        self.y=b
+#p=Point(3,5)
+#print(p.x)
+#print(p.y)
+
+class Flight():
+    def __init__(self,capacity):
+        self.capacity=capacity
+        self.passengers=[]
+
+    def add_passengers(self,name):
+        if not self.open_seats():
+            return False
+        self.passengers.append(name)
+        return True
+
+    def open_seats(self):
+        return self.capacity - len(self.passengers)
+        
+flight=Flight(2)
+
+p=["Sarmad","Malaika","Ali","Kashif"]
+for person in p:
+    if flight.add_passengers(person):
+        print(f"Added {person} to the flight ")
+    else:
+        print(f"No available seats for {person}")
